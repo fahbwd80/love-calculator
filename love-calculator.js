073c22d01c270f6f99b08985ae0424883699dcac
Fahb <moh.hasan0909@gmail.com>
2023-09-08 11:11:09 +0800

    love-calculator

---
+++ love-calculator.js
@@ -0,0 +1,20 @@
+function love(){
+var name1 = document.getElementById("NameOne").value;
+var name2 = document.getElementById("NameTwo").value;
+if(name1.lenght<=2){
+    alert("Enter at least 3 character");
+}
+if(name2.lenght<=2){
+    alert("Enter at least 3 character");
+}
+else{
+    var random = Math.floor(Math.random()*100)
+    document.getElementById("print").innerHTML = name1 + "and " + name2 + " = "  +random + "%" + " of love"
+    if(random<=10 || random<=50){
+        document.getElementById("statement").innerHTML = "Good Ralationship";
+    }
+    else if(random<=51 || random<=100){
+        document.getElementById("statement").innerHTML = "Lovely Ralationship";
+    }
+ }
+}
